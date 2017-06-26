b181f85a154f1e36620767e3438ccec43d68aaef
James Melville <jlmelville@gmail.com>
2017-06-25 18:59:10 -0700

    Initializing wolfe line search, clears all old data. Add test that reinitializing gives same results.

--- tests/testthat/test_loop_api.R
+++ tests/testthat/test_loop_api.R
@@ -36,3 +36,36 @@ test_that("can initialize in make_mize if par and fg are to hand", {
   expect_equal(norm2(rosenbrock_fg$gr(par)), 147.11, tol = 1e-3)
   expect_equal(res$par, c(-1.144, 1.023), tol = 1e-3)
 })
+
+test_that("reinitializing produces the same results", {
+  opt <- make_mize(method = "BFGS", line_search = "more-thuente")
+
+  opt <- mize_init(opt, rb0, rosenbrock_fg)
+  par <- rb0
+  for (iter in 1:3) {
+    res <- mize_step(opt, par, rosenbrock_fg)
+    par <- res$par
+    opt <- res$opt
+  }
+
+  expect_equal(res$nf, 5)
+  expect_equal(res$ng, 5)
+  expect_equal(rosenbrock_fg$fn(par), 4.28, tol = 1e-3)
+  expect_equal(norm2(rosenbrock_fg$gr(par)), 17.29, tol = 1e-3)
+  expect_equal(res$par, c(-1.048, 1.070), tol = 1e-3)
+
+  opt <- mize_init(opt, rb0, rosenbrock_fg)
+  par <- rb0
+  for (iter in 1:3) {
+    res <- mize_step(opt, par, rosenbrock_fg)
+    par <- res$par
+    opt <- res$opt
+  }
+
+  # nf and ng are remembered
+  expect_equal(res$nf, 10)
+  expect_equal(res$ng, 10)
+  expect_equal(rosenbrock_fg$fn(par), 4.28, tol = 1e-3)
+  expect_equal(norm2(rosenbrock_fg$gr(par)), 17.29, tol = 1e-3)
+  expect_equal(res$par, c(-1.048, 1.070), tol = 1e-3)
+})
